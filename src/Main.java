ebb57bb8695039a3c7f20b32539bb0f5f2069258
Neto test <svietlana.kabanova@bk.ru>
2024-09-28 14:33:37 +1100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int purchase = 7000; // Стоимость билета
+        int bonusMiles = (int) (purchase / 20); // Начисленные мили
+
+        System.out.println("Начисленные мили:" + bonusMiles);
+    }
+}
